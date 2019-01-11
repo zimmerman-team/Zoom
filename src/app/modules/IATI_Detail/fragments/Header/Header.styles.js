9abdeea8dc6a0dc3341599e9235ae33d573ee98d
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-01-11 15:36:33 +0200

    Added unit test for Iati page Header component

---
+++ src/app/modules/IATI_Detail/fragments/Header/Header.styles.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import { Box } from 'grommet';
+import {
+  zoomFontFamOne,
+  zoomFontFamTwo,
+  aidsFondsBlue,
+} from 'components/theme/ThemeSheet';
+
+// const ComponentBase = styled.div``;
+
+export const DetailList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 100%;
+`;
+
+export const DetailListItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  width: 25%;
+  margin-bottom: 30px;
+`;
+
+export const ItemLabel = styled.div`
+  color: #000;
+  font-family: ${zoomFontFamOne};
+  line-height: 1;
+  margin-bottom: 9px;
+`;
+export const ItemInfo = styled.div`
+  font-family: ${zoomFontFamTwo};
+  color: ${aidsFondsBlue};
+  line-height: 1;
+  position: relative;
+`;
+
+export const Tooltip = styled(Box)`
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 250px;
+  background-color: white;
+  padding: 20px;
+  z-index: 2;
+  border-radius: 2%;
+  font-family: ${zoomFontFamTwo};
+  line-height: 1.3;
+`;

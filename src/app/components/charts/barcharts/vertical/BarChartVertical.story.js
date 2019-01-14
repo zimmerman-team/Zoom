cb2ecd2412bf452ca8e3ab9a188dfc4fc1aec2ed
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-01-14 12:32:32 +0200

    minor fix

--- src/app/components/charts/barcharts/vertical/BarChartVertical.story.js
+++ src/app/components/charts/barcharts/vertical/BarChartVertical.story.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import BarChartVertical from './BarChartVertical';
-import { barChartMockData } from '__mocks__/barChartVerticalMock';
+import { IATIDetailBarChartMockData } from '__mocks__/barChartVerticalMock';
 
 storiesOf('Charts', module).add('Bar Chart Vertical', () => (
-  <BarChartVertical data={barChartMockData} />
+  <BarChartVertical data={IATIDetailBarChartMockData} />
 ));

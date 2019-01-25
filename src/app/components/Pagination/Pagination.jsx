78eeb25d6c0d2bae2e5c90957d127a2c894f4fe0
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-01-25 16:34:32 +0200

    Implement Create team view logic

--- src/app/components/Pagination/Pagination.jsx
+++ src/app/components/Pagination/Pagination.jsx
@@ -1,24 +1,40 @@
+/* base */
 import React from 'react';
+import PropTypes from 'prop-types';
 import ReactPaginate from 'react-paginate';
+
+/* style */
 import './Pagination.css';
 
-const Pagination = () => (
+const propTypes = {
+  pageCount: PropTypes.number,
+  changePage: PropTypes.func,
+};
+const defaultProps = {
+  pageCount: 0,
+  changePage: null,
+};
+
+const Pagination = props => (
   <ReactPaginate
-    previousLabel={'previous'}
-    nextLabel={'next'}
-    breakLabel={'...'}
-    breakClassName={'break-me'}
-    pageCount={5}
+    previousLabel="previous"
+    nextLabel="next"
+    breakLabel="..."
+    breakClassName="break-me"
+    pageCount={props.pageCount}
     marginPagesDisplayed={2}
     pageRangeDisplayed={5}
-    onPageChange={(e) => console.log('Selected page', e)}
-    containerClassName={'pagination'}
-    pageClassName={'page'}
-    activeClassName={'active-page'}
-    nextClassName={'nextPage'}
-    previousClassName={'prevPage'}
-    disabledClassName={'disabledLab'}
+    onPageChange={props.changePage}
+    containerClassName="pagination"
+    pageClassName="page"
+    activeClassName="active-page"
+    nextClassName="nextPage"
+    previousClassName="prevPage"
+    disabledClassName="disabledLab"
   />
 );
+
+Pagination.propTypes = propTypes;
+Pagination.defaultProps = defaultProps;
 
 export default Pagination;
--- src/app/mediators/ModuleMediators/CreateTeamMediator/CreateTeamMediator.js
+++ src/app/mediators/ModuleMediators/CreateTeamMediator/CreateTeamMediator.js
@@ -1,8 +1,10 @@
 /* base */
 import React from 'react';
+import filter from 'lodash/filter';
 
 /* components */
 import CreateTeamModule from 'modules/UserManagement/CreateTeam/CreateTeamModule';
+import { formatUsersData } from './CreateTeamMediator.utils';
 
 class CreateTeamMediator extends React.Component {
   constructor(props) {
@@ -15,11 +17,41 @@ class CreateTeamMediator extends React.Component {
       name: '',
       searchKeyword: '',
       users: [],
+      allUsers: [],
+      sort: 'name:1',
+      page: 0,
+      totalPages: 0,
     };
 
+    this.setUsers = this.setUsers.bind(this);
     this.submitForm = this.submitForm.bind(this);
+    this.changePage = this.changePage.bind(this);
     this.changeName = this.changeName.bind(this);
+    this.getAllUsers = this.getAllUsers.bind(this);
+    this.addRemoveUser = this.addRemoveUser.bind(this);
     this.changeSearchKeyword = this.changeSearchKeyword.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllUsers();
+  }
+
+  getAllUsers() {
+    this.props.auth0Client.getAllUsers(
+      this.setUsers,
+      this.state.page,
+      this.state.sort,
+      this.state.searchKeyword !== ''
+        ? ` AND name:${this.state.searchKeyword}*`
+        : '',
+    );
+  }
+
+  setUsers(data) {
+    this.setState({
+      allUsers: formatUsersData(data),
+      totalPages: Math.ceil(data.total / 10),
+    });
   }
 
   changeName(e) {
@@ -28,14 +60,41 @@ class CreateTeamMediator extends React.Component {
     });
   }
 
+  changePage(e) {
+    this.setState(
+      {
+        page: e.selected,
+      },
+      () => {
+        this.getAllUsers();
+      },
+    );
+  }
+
   changeSearchKeyword(e) {
-    this.setState({
-      searchKeyword: e.target.value,
-    });
+    this.setState(
+      {
+        searchKeyword: e.target.value,
+      },
+      () => {
+        this.getAllUsers();
+      },
+    );
+  }
+
+  addRemoveUser(e) {
+    let values = this.state.users;
+    if (!e.target.checked) {
+      values = filter(values, v => v !== e.target.id);
+    } else {
+      values.push(e.target.id);
+    }
+    this.setState({ users: values });
   }
 
   submitForm(e) {
     e.preventDefault();
+    this.props.auth0Client.addGroup(this.state.name, this.state.users, this);
   }
 
   render() {
@@ -43,12 +102,16 @@ class CreateTeamMediator extends React.Component {
       <CreateTeamModule
         users={this.state.users}
         name={this.state.name}
+        userOptions={this.state.allUsers}
         changeName={this.changeName}
-        searchKeyword={this.state.searchKeyword}
+        totalPages={this.state.totalPages}
         changeSearchKeyword={this.changeSearchKeyword}
         success={this.state.success}
         secondaryInfoMessage={this.state.secondaryInfoMessage}
         errorMessage={this.state.errorMessage}
+        addRemoveUser={this.addRemoveUser}
+        changePage={this.changePage}
+        submitForm={this.submitForm}
       />
     );
   }

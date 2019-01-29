c104f22e604fab51c0a1cf03978d071781d39a85
Jim Fung <jim@zimmermanzimmerman.nl>
2019-01-29 18:12:48 +0100

    Sorted some storybook stories
    Signed-off-by: Jim Fung <jim@zimmermanzimmerman.nl>

--- src/app/components/RadioButtonGroup/RadioButtonGroup.story.js
+++ src/app/components/RadioButtonGroup/RadioButtonGroup.story.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import RadioButtonGroup from './RadioButtonGroup';
 
-storiesOf('Components', module).add('RadioButtonGroup', () => (
+storiesOf('Component groups', module).add('RadioButtonGroup', () => (
   <RadioButtonGroup />
 ));
--- src/app/modules/home/HomeModule.story.js
+++ src/app/modules/home/HomeModule.story.js
@@ -2,4 +2,4 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import HomeModule from './HomeModule';
 
-storiesOf('Modules', module).add('Home', () => <HomeModule />);
+storiesOf('Module: Home', module).add('Home', () => <HomeModule />);

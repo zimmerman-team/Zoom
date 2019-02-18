7ff536615cde58ec876f4cca26473ecdc6852c55
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-02-18 14:17:51 +0200

    Refactor country detail bar chart

--- src/app/modules/countrydetail/fragments/CountryInfo/CountryInfo.test.js
+++ src/app/modules/countrydetail/fragments/CountryInfo/CountryInfo.test.js
@@ -3,11 +3,11 @@ import { shallow } from 'enzyme';
 import CountryInfo from './CountryInfo';
 // Components
 import { Box } from 'grommet';
-import BarChartHorizontal from 'components/charts/barcharts/horizontal/BarChartHorizontal';
+import HorizontalBarChart from 'components/charts/barcharts/horizontal/HorizontalBarChart';
 import { SimpleText } from 'components/sort/Misc';
 import {
   PageIntroInitial,
-  PageIntroSecondary,
+  PageIntroSecondary
 } from 'components/sort/Paragraphs';
 import ModuleFragment from 'components/Layout/ModuleFragment/ModuleFragment';
 import { CountryName } from 'modules/countrydetail/fragments/CountryInfo/CountryInfo.styles';
@@ -33,7 +33,7 @@ describe('<CountryInfo />', () => {
   it('renders one <SimpleText/> component', () => {
     expect(wrapper.find(SimpleText)).toHaveLength(1);
   });
-  it('renders one <BarChartHorizontal/> component', () => {
-    expect(wrapper.find(BarChartHorizontal)).toHaveLength(1);
+  it('renders one <HorizontalBarChart/> component', () => {
+    expect(wrapper.find(HorizontalBarChart)).toHaveLength(1);
   });
 });

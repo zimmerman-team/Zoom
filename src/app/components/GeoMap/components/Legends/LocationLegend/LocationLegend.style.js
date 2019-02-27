0ba63320fc0a63f907f36328f40cafe2207a7ce4
fus <martynas@zimmermanzimmerman.nl>
2019-02-27 19:30:03 +0200

    Location showing on the map done. But very not hut.
    Signed-off-by: fus <martynas@zimmermanzimmerman.nl>

---
+++ src/app/components/GeoMap/components/Legends/LocationLegend/LocationLegend.style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { LegendItem } from 'components/GeoMap/components/Legends/Legend.styles';
+
+export const LocationLegendItem = styled(LegendItem)`
+  width: fit-content;
+`;

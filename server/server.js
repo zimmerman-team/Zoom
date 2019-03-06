c504c175fb30429d4425b89ceaeb1542578516a2
fus <martynas@zimmermanzimmerman.nl>
2019-03-06 11:32:13 +0200

    Okay was easier then expected. Added mongodb to project.
    Signed-off-by: fus <martynas@zimmermanzimmerman.nl>

--- server/server.js
+++ server/server.js
@@ -4,6 +4,16 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const PORT = 4200;
 const cors = require('cors');
+const config = require('./database/DB');
+
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => {
+    console.log('Database is connected');
+  },
+  err => {
+    console.log('Can not connect to the database' + err);
+  }
+);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));

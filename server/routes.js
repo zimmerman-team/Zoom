782ac48c0482a907f5a16ca9cde74262730a1d2a
fus <martynas@zimmermanzimmerman.nl>
2019-03-07 16:41:41 +0200

    routes and controllers in progres...
    Signed-off-by: fus <martynas@zimmermanzimmerman.nl>

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,7 @@
-import Chart from './api/Chart';
+import Chart from './controllers/Chart';
 
-module.exports = function(app) {
-  app.use('/api/chart', Chart);
-};
+const express = require('express');
+const router = express.Router();
+
+// Require controller modules.
+const chartController = require('./controllers/Chart');

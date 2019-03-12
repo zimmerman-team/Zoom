1c3d0a054a41a33c6807b793c6b95e6f424ccd41
Jim Fung <jim@zimmermanzimmerman.nl>
2019-03-12 15:05:00 +0100

    incremental commit
    Signed-off-by: Jim Fung <jim@zimmermanzimmerman.nl>

--- src/app/components/TempDrawer/TempDrawer.jsx
+++ src/app/components/TempDrawer/TempDrawer.jsx
@@ -2,88 +2,154 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 /* material ui */
-import { makeStyles } from '@material-ui/styles';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import IconClose from 'assets/icons/IconClose';
+import theme from 'theme/Theme';
+import { data } from './TempDrawer.const';
+import IconCharts from 'assets/icons/IconCharts';
+import LoginForm from 'components/SideBar/comps/LoginForm/LoginForm';
 
-/* project */
+export const CloseButton = styled.button`
+  padding: 0;
+  color: ${theme.color.aidsFondsRed};
+  font-family: ${theme.font.zoomFontFamTwo};
+  font-size: 14px;
+`;
 
-/** materual ui styling */
-const useStyles = makeStyles({
-  list: {
-    width: 320
+const LoginBox = styled.div`
+  width: 320px;
+`;
+
+const SidebarNavList = styled(List)`
+  && {
+    width: 320px;
+    background-color: ${theme.color.zoomGreyZero};
+    padding: 0;
+    padding-top: 10px;
+    padding-bottom: 10px;
   }
-});
+`;
 
-/** styled comps styling */
+const SidebarNavListItem = styled(ListItem)`
+  && {
+    display: flex;
+    padding-top: 6px;
+    padding-bottom: 6px;
+    line-height: 1;
+  }
+`;
+
+const SidebarClosButton = styled(SidebarNavListItem)`
+  height: 40px;
+`;
+
+const ZoomListItemText = styled(ListItemText)`
+  && {
+    padding: 0;
+    span {
+      color: ${theme.color.aidsFondsRed};
+      font-family: ${theme.font.zoomFontFamTwo};
+      font-size: 14px;
+    }
+  }
+`;
+
+const ZoomLink = styled(Link)`
+  display: flex;
+  text-decoration: none;
+`;
 
 const propTypes = {
   open: PropTypes.bool,
-  toggleSideBar: PropTypes.func
+  toggleSideBar: PropTypes.func,
+  items: PropTypes.array
 };
 const defaultProps = {
-  open: undefined
+  open: undefined,
+  items: data
 };
 
-function TempDrawer() {
-  const classes = useStyles();
-  const [state, setState] = React.useState({
-    left: true
-  });
-
-  const toggleDrawer = (side, open) => () => {
-    setState({ ...state, [side]: open });
+class TempDrawer extends React.Component {
+  state = {
+    open: this.props.open
   };
 
-  const sideList = (
-    <div className={classes.list}>
-      <List>
-        {[
-          'Home',
-          'Country Detail',
-          'IATI Detail',
-          'Datamapper',
-          'NL Focus',
-          'Visualizer',
-          'About ZOOM'
-        ].map(text => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              <MailIcon />
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
+  render() {
+    const sideList = (
+      <React.Fragment>
+        {data.map(item => (
+          <SidebarNavListItem
+            button
+            key={item.label}
+            data-cy={'sidebar-' + item.label}
+            disableRipple
+          >
+            <ZoomLink to={item.path}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ZoomListItemText primary={item.label} />
+            </ZoomLink>
+          </SidebarNavListItem>
         ))}
-      </List>
-      <Divider />
-    </div>
-  );
+      </React.Fragment>
+    );
 
-  return (
-    <React.Fragment>
-      <Button onClick={toggleDrawer('left', true)}>Open Left</Button>
+    return (
+      <React.Fragment>
+        <Drawer open={this.props.open} onClose={this.props.toggleSideBar}>
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.props.toggleSideBar}
+            onKeyDown={this.props.toggleSideBar}
+          >
+            <SidebarClosButton
+              button
+              onClick={this.props.toggleSideBar}
+              data-cy="sidebar-close"
+              disableRipple
+            >
+              <ListItemIcon>
+                <IconClose />
+              </ListItemIcon>
+              <ZoomListItemText primary="Close" />
+            </SidebarClosButton>
+            <SidebarNavList>
+              {sideList}
+              {this.props.auth0Client &&
+                (this.props.auth0Client.isAuthenticated() &&
+                  this.props.auth0Client.isAdministrator() && (
+                    <ZoomLink to="/dashboard">
+                      <SidebarNavListItem
+                        label="Dashboard"
+                        onClick={this.props.toggleSideBar}
+                        icon={<IconCharts />}
+                        button
+                        data-cy="sidebar-dashboard"
+                      >
+                        <ListItemIcon>
+                          <IconClose />
+                        </ListItemIcon>
+                        <ZoomListItemText primary="Close" />
+                      </SidebarNavListItem>
+                    </ZoomLink>
+                  ))}
+            </SidebarNavList>
+          </div>
 
-      <Drawer open={state.left} onClose={toggleDrawer('left', false)}>
-        <div
-          tabIndex={0}
-          role="button"
-          onClick={toggleDrawer('left', false)}
-          onKeyDown={toggleDrawer('left', false)}
-        >
-          {sideList}
-        </div>
-      </Drawer>
-    </React.Fragment>
-  );
+          <LoginBox>
+            <LoginForm auth0Client={this.props.auth0Client} />
+          </LoginBox>
+        </Drawer>
+      </React.Fragment>
+    );
+  }
 }
 
 TempDrawer.propTypes = propTypes;

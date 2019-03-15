3b8fe520a1f927ad05236d44569e3dfc053b0732
juanalb <juanalbergen@gmail.com>
2019-03-15 11:35:19 +0100

    fix

--- src/app/components/Select/ZoomSelect.jsx
+++ src/app/components/Select/ZoomSelect.jsx
@@ -230,6 +230,7 @@ class ZoomSelect extends React.Component {
             })
           }
         />
+        {console.log(this.props.placeHolderText, this.props.placeHolderNumber)}
         {this.state.open && (
           <DropDownContainer>
             {this.state.options.length > 0 ? (

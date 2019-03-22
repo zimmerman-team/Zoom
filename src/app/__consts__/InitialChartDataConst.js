a6707fcbe5aa6118686bb7acbe730220f2adaabc
juanalb <juan@zimmermanzimmerman.nl>
2019-03-22 10:29:43 +0100

    feature/ZOOM-1543 (#156)
    * Trailling commas,,,,,
    
    Signed-off-by: juanalb <juanalbergen@gmail.com>
    
    * Changed year selector intial value to 2003, 2016
    
    Signed-off-by: juanalb <juanalbergen@gmail.com>

--- src/app/__consts__/InitialChartDataConst.js
+++ src/app/__consts__/InitialChartDataConst.js
@@ -2,12 +2,12 @@ import { formatYearParam } from 'utils/genericUtils';
 
 // As discussed with Siem default year period selected should be
 // current year and 15 years before
-const now = new Date();
-const currentYear = now.getFullYear();
-const yearBefore = currentYear - 15;
+// const now = new Date();
+// const currentYear = now.getFullYear();
+// const yearBefore = currentYear - 15;
 
 const initialState = {
-  yearPeriod: formatYearParam([yearBefore, currentYear]),
+  yearPeriod: formatYearParam([2003, 2016]),
   indicators: [],
   selectedInd1: undefined,
   selectedInd2: undefined,

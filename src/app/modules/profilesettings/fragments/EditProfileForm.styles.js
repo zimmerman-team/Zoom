ac077617bc619be326d7a6abbb4d3acfbd658d2d
juanalb <juan@zimmermanzimmerman.nl>
2019-03-22 10:22:32 +0100

    feature/ZOOM-915 (#154)
    * ZOOM-1417 changes implemented
    
    Signed-off-by: juanalb <juanalbergen@gmail.com>
    
    * EDIT PROFILE FORM
    UI as design
    
    Signed-off-by: juanalb <juanalbergen@gmail.com>
    
    * merge conflict
    
    Signed-off-by: Jim Fung <jim@zimmermanzimmerman.nl>

---
+++ src/app/modules/profilesettings/fragments/EditProfileForm.styles.js
@@ -0,0 +1,17 @@
+import theme from 'theme/Theme';
+import styled from 'styled-components';
+import TextField from '../../../components/sort/TextField';
+
+export const ComponentBase = styled.div`
+  width: 1024px;
+`;
+
+export const Textfield = styled(TextField)`
+  && input {
+    color: ${theme.color.aidsFondsBlue};
+  }
+
+  &:not(:first-child) {
+    margin-top: 16px;
+  }
+`;

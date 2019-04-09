76b54a2a4a00e9c61280392831542031a5b237d4
Jim Fung <jim@zimmermanzimmerman.nl>
2019-04-09 17:22:36 +0200

    Resolved ZOOM-1628: Hide dashboard item in sidebar when user is not signed in
    Signed-off-by: Jim Fung <jim@zimmermanzimmerman.nl>

---
+++ src/app/components/MainMenuDrawer/common/SidebarNavListItem.jsx
@@ -0,0 +1,27 @@
+/* base */
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import ListItem from '@material-ui/core/ListItem';
+
+const propTypes = {
+  type: PropTypes.string,
+  loggedIn: PropTypes.bool
+};
+
+const SidebarNavListItem = styled(props => {
+  const show =
+    (props.type === 'private' && props.loggedIn) || props.type === 'public';
+  return show && <ListItem button disableRipple {...props} />;
+})`
+  && {
+    display: flex;
+    padding-top: 6px;
+    padding-bottom: 6px;
+    line-height: 1;
+  }
+`;
+
+SidebarNavListItem.propTypes = propTypes;
+
+export default SidebarNavListItem;

981cdb4fe699268a4954b49c399530d8b4300f70
Jim Fung <jim@zimmermanzimmerman.nl>
2019-04-10 17:47:36 +0200

    incremental
    Signed-off-by: Jim Fung <jim@zimmermanzimmerman.nl>

---
+++ src/app/components/SimpleSelect/SimpleSelect.jsx
@@ -0,0 +1,24 @@
+/* base */
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import theme from 'theme/Theme';
+
+/**
+ * todo: Please write a short component description of what this component does
+ * @param {Object} customProperty - please describe component property
+ */
+
+const ComponentBase = styled.div``;
+
+const propTypes = {};
+const defaultProps = {};
+
+const SimpleSelect = props => {
+  return <ComponentBase />;
+};
+
+SimpleSelect.propTypes = propTypes;
+SimpleSelect.defaultProps = defaultProps;
+
+export default SimpleSelect;

5b67ef21b936c9b1f784133a3036849f4b4be6ee
fus <martynas@zimmermanzimmerman.nl>
2019-04-11 17:54:57 +0300

    Barchart saving/editing/viewing IMPLEMENTED!

--- src/app/mediators/ModuleMediators/VisualizerModuleMediator/VisualizerModuleMediator.utils.js
+++ src/app/mediators/ModuleMediators/VisualizerModuleMediator/VisualizerModuleMediator.utils.js
@@ -10,6 +10,15 @@ const lineChartColors = [
   'hsl(84, 70%, 50%)'
 ];
 
+const barChartColors = [
+  'hsl(265, 70%, 50%)',
+  'hsl(323, 70%, 50%)',
+  'hsl(144, 70%, 50%)',
+  'hsl(91, 70%, 50%)',
+  'hsl(28, 70%, 50%)',
+  'hsl(275, 70%, 50%)'
+];
+
 // Updates layer percentiles depending on the value
 export function updatePercentiles(featureCollection, accessor) {
   const { features } = featureCollection;
@@ -370,4 +379,80 @@ export function formatLineData(indicators) {
   });
 
   return indicatorData;
+}
+
+// so this function basically formats the
+// keys for certain types of charts(like bar chart)
+// according to the selected indicator
+// array passed into it
+export function formatKeys(selectedInd) {
+  const chartKeys = [];
+
+  selectedInd.forEach((indName, index) => {
+    // this if is here so we dont push 'undefined' as a key
+    if (indName) {
+      let key = indName;
+
+      if (chartKeys.indexOf(indName) !== -1)
+        key = indName.concat(` (${index})`);
+
+      chartKeys.push(key);
+    }
+  });
+
+  return chartKeys;
+}
+
+export function formatBarData(indicators) {
+  const barChartData = [];
+  const barChartKeys = [];
+
+  let colorInd = 0;
+  indicators.map((indicator, index) => {
+    if (indicator.length > 0) {
+      const existInd = barChartKeys.indexOf(indicator[0].indicatorName);
+
+      let indName = indicator[0].indicatorName;
+
+      // so we need this logic for when a person would
+      // plot two indicators with the same name
+      // as the id needs to be unique, we just add
+      // the index as a suffix
+      if (existInd !== -1) indName = indName.concat(` (${index})`);
+
+      barChartKeys.push(indName);
+
+      indicator.forEach(indItem => {
+        // yeah and cause we might receive data with the same geolocation name
+        // we add in the values for that geolocation so it wouldn't be repeated over and over
+        const existItemInd = findIndex(barChartData, existing => {
+          return indItem.geolocationTag === existing.geoName;
+        });
+
+        if (existItemInd === -1)
+          barChartData.push({
+            geoName: indItem.geolocationTag,
+
+            geolocation:
+              indItem.geolocationIso2.length > 0
+                ? indItem.geolocationIso2.toUpperCase()
+                : indItem.geolocationTag,
+
+            [indName]: Math.round(indItem.value),
+            [`${indName}Color`]: barChartColors[colorInd]
+          });
+        else if (barChartData[existItemInd][indName] !== undefined)
+          barChartData[existItemInd][indName] += Math.round(indItem.value);
+        else {
+          barChartData[existItemInd][indName] = Math.round(indItem.value);
+          barChartData[existItemInd][`${indName}Color`] =
+            barChartColors[colorInd];
+        }
+      });
+
+      if (colorInd + 1 < lineChartColors.length) colorInd += 1;
+    }
+  });
+
+  return barChartData;
 }

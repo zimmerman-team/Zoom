cdcf2bf3866672c19bd37702fa70781b40895461
Jim Fung <jim@zimmermanzimmerman.nl>
2019-04-11 19:13:22 +0200

    Added the graph structure panel
    Signed-off-by: Jim Fung <jim@zimmermanzimmerman.nl>

--- src/app/components/Panes/DataExplorePane/panels/GraphStructurePanel/sort/SimpleSelect/SimpleSelect.story.js
+++ src/app/components/Panes/DataExplorePane/panels/GraphStructurePanel/sort/SimpleSelect/SimpleSelect.story.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import SimpleSelect from './SimpleSelect';
+import SimpleSelect from 'components/Panes/DataExplorePane/panels/GraphStructurePanel/sort/SimpleSelect/SimpleSelect';
 
 storiesOf('Components', module).add('SimpleSelect', () => <SimpleSelect />);

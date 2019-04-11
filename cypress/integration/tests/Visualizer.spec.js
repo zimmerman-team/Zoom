f169ff7569792668145961a901bd602295d93e65
juanalb <juanalbergen@gmail.com>
2019-04-11 11:56:01 +0200

    ZOOM-1406: Chartbuilder geomap E2E
    Signed-off-by: juanalb <juanalbergen@gmail.com>

--- cypress/integration/tests/Visualizer.spec.js
+++ cypress/integration/tests/Visualizer.spec.js
@@ -13,7 +13,11 @@ function signIn() {
   cy.get('[data-cy=sidebar-login-button]').click();
   cy.get('[data-cy="dialog-overlay"]').click();
 }
-
+function navigateToCreateGeo() {
+  cy.get('[data-cy="appbar-right-button"]').click();
+  cy.get('[data-cy="nav-pane-item-0"]').click();
+  cy.get('[data-cy="nav-pane-item-0"]').click();
+}
 describe('Create geo functionality', function() {
   it("Shouldn't be able to create geo when not logged in", function() {
     cy.visit('/');
@@ -45,31 +49,61 @@ describe('Create geo functionality', function() {
 });
 
 describe('Chartbuilder geomap chart fragment e2e', function() {
-  it('Should pass self written text from the /context to /preview', function() {
+  it('Should pass written text from the /context to /preview', function() {
     signIn();
-    cy.get('[data-cy="appbar-right-button"]').click();
-    cy.get('[data-cy="nav-pane-item-0"]').click();
-    //Fixme: Change to proper url.
+    navigateToCreateGeo();
+    // Fixme: Change to proper url or simultate click of the button.
     cy.visit('/visualizer/geomap/vizID/context');
     cy.get('textarea')
       .last()
       .type('This is a test');
     cy.get('[href="/visualizer/geomap/vizID/preview"]').click();
-    // cy.get('[data-cy="context-preview-body"]').should(
-    //   'have.text',
-    //   'This is a test'
-    // );
+    cy.get('[data-cy="context-preview-intro"]').should(
+      'have.text',
+      'This is a test'
+    );
   });
 
-  //it('Should display mapped data', function() {});
+  it('Should map aids related deaths data on the geo map', function() {
+    signIn();
+    navigateToCreateGeo();
+    cy.contains('Select indicator').click();
+    cy.contains('aids related deaths (unaids)').click();
+    cy.get('[data-cy="legendLayer-label"]').should(
+      'contain',
+      'aids related deaths (unaids)'
+    );
+  });
 
-  // it('Should "save" the chart to the dashboard', function() {});
-  //
-  // it('Should "save" the chart as a JSON, CSV or XML file ', function() {});
-  //
-  // it('Should duplicate chart to dashboard', function() {});
-  //
-  // it('Should publish the chart to public zoom library', function() {});
-  //
-  // it('Should publish the chart to the team', function() {});
+  it('Should "save" the chart to the dashboard', function() {
+    signIn();
+    navigateToCreateGeo();
+    cy.get('[data-cy="legendLayer-label"]').should(
+      'contain',
+      'aids related deaths'
+    );
+    cy.get('[data-cy=geomap-close-save-button]').click();
+    // Fixme: Should check on the vizID in the url,
+    // however on creating a chart, the vizID is not in the URL yet.
+  });
+
+  it('Should be able to download to csv/xml/json ', function() {
+    signIn();
+    navigateToCreateGeo();
+    cy.visit('/visualizer/geomap/vizID/download');
+    cy.get('[data-cy="dowload-option-JSON"]').click();
+    cy.get('[data-cy="dowload-option-CSV"]').click();
+    cy.get('[data-cy="dowload-option-XML"]').click();
+    // Fixme: Do something (dont know expected behaviour yet
+  });
+
+  it('Should publish the chart to public zoom library', function() {
+    signIn();
+    navigateToCreateGeo();
+    cy.visit('/visualizer/geomap/vizID/visibility');
+    cy.get('[data-cy="publish-chart-to-public"]').click();
+    cy.visit('/public/chart-library');
+    // Fixme: Should check on the vizID in the url,
+    // however on creating a chart, the vizID is not in the URL yet.
+  });
 });

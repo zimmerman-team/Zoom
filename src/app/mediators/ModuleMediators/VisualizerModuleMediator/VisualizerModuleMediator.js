0f8fe611f41a44095ebd2ad7dc58b6657d208dfc
fus <martynas@zimmermanzimmerman.nl>
2019-04-15 12:51:24 +0300

    Adjusted the iso2 NUll value filtering

--- src/app/mediators/ModuleMediators/VisualizerModuleMediator/VisualizerModuleMediator.js
+++ src/app/mediators/ModuleMediators/VisualizerModuleMediator/VisualizerModuleMediator.js
@@ -396,6 +396,10 @@ class VisualizerModuleMediator extends Component {
       regionCountriesCodes
     );
 
+    // so this variable basically controlls the filter param for data points
+    // that don't have/do have geolocationIso2 field
+    const iso2Undef = countriesISO2.indexOf('undefined') !== -1;
+
     const refetchVars = {
       indicator1: [ind1],
       indicator2: [ind2],
@@ -404,7 +408,8 @@ class VisualizerModuleMediator extends Component {
       singleInd2: ind2 || 'null',
       datePeriod: [selectedYear],
       subInd1: subInd1.length > 0 ? subInd1 : ['undefined'],
-      subInd2: subInd2.length > 0 ? subInd2 : ['undefined']
+      subInd2: subInd2.length > 0 ? subInd2 : ['undefined'],
+      OR_GeolocationIso2_Is_Null: iso2Undef
     };
 
     this.props.relay.refetch(refetchVars, null, () =>
@@ -468,6 +473,7 @@ export default createRefetchContainer(
         countriesISO2: { type: "[String]", defaultValue: ["null"] }
         singleInd1: { type: "String", defaultValue: "null" }
         singleInd2: { type: "String", defaultValue: "null" }
+        OR_GeolocationIso2_Is_Null: { type: "Boolean", defaultValue: true }
       ) {
       indicators1: datapointsAggregation(
         groupBy: [
@@ -485,6 +491,7 @@ export default createRefetchContainer(
         indicatorName_In: $indicator1
         geolocationIso2_In: $countriesISO2
         filterName_In: $subInd1
+        OR_GeolocationIso2_Is_Null: $OR_GeolocationIso2_Is_Null
       ) {
         indicatorName
         geolocationIso2
@@ -511,6 +518,7 @@ export default createRefetchContainer(
         indicatorName_In: $indicator2
         geolocationIso2_In: $countriesISO2
         filterName_In: $subInd2
+        OR_GeolocationIso2_Is_Null: $OR_GeolocationIso2_Is_Null
       ) {
         indicatorName
         geolocationIso2
@@ -547,6 +555,7 @@ export default createRefetchContainer(
       $countriesISO2: [String]!
       $singleInd1: String!
       $singleInd2: String!
+      $OR_GeolocationIso2_Is_Null: Boolean!
     ) {
       ...VisualizerModuleMediator_indicatorAggregations
         @arguments(
@@ -558,6 +567,7 @@ export default createRefetchContainer(
           singleInd2: $singleInd2
           subInd1: $subInd1
           subInd2: $subInd2
+          OR_GeolocationIso2_Is_Null: $OR_GeolocationIso2_Is_Null
         )
     }
   `

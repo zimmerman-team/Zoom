b9030b33e6bf1cd7f9f240fd9394d7e5eba41f5c
Jim Fung <jim@zimmermanzimmerman.nl>
2019-04-18 18:21:51 +0200

    incremental
    Signed-off-by: Jim Fung <jim@zimmermanzimmerman.nl>

--- src/app/modules/visualizer/sort/sidebar/VizSidebar.style.js
+++ src/app/modules/visualizer/sort/sidebar/VizSidebar.style.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
 export const ComponentBase = styled.div`
-  position: fixed;
-  right: 0;
-  top: 40px;
-  z-index: 9;
+  //position: fixed;
+  //right: 0;
+  //top: 40px;
+  width: 320px;
+  flex-shrink: 0;
+  //z-index: 9;
   display: flex;
   flex-direction: column;
   border-radius: 0;

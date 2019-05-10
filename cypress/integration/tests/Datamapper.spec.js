1c7b20984ee9b68befe40470fb8e300fdf8740b2
juanalb <juanalbergen@gmail.com>
2019-05-10 10:06:39 +0200

    Deleted unused library
    Signed-off-by: juanalb <juanalbergen@gmail.com>

--- cypress/integration/tests/Datamapper.spec.js
+++ cypress/integration/tests/Datamapper.spec.js
@@ -1,9 +1,6 @@
-import 'cypress-file-upload';
-
 beforeEach(() => {
   // README keep in mind that Cypress clears the whole state before each test. => signIn() before each test.
   // set this for skipping landing dialog
-
   cy.setCookie('cookieNotice', 'false');
 });
 

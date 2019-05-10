ed629981b5a494870fb9f9f9897e96366f1b9c50
juanalb <juanalbergen@gmail.com>
2019-05-10 15:19:00 +0200

    Prettier doing some modifications
    Signed-off-by: juanalb <juanalbergen@gmail.com>

--- src/app/modules/countrydetail/CountryDetailModule.jsx
+++ src/app/modules/countrydetail/CountryDetailModule.jsx
@@ -8,7 +8,7 @@ import { Helmet } from 'react-helmet';
 
 import CountryInfo from 'modules/countrydetail/fragments/CountryInfo/CountryInfo';
 import HumanRights from 'modules/countrydetail/fragments/HumanRights';
-import CivicSpace from 'app/modules/countrydetail/fragments/CivicSpace/CivicSpace';
+import CivicSpace from 'modules/countrydetail/fragments/CivicSpace/CivicSpace';
 import AidsEpidemic from 'modules/countrydetail/fragments/AidsEpidemic/AidsEpidemic';
 import EconomicIndicators from 'modules/countrydetail/fragments/EconomicIndicators';
 import AidsfondsTransactions from 'modules/countrydetail/fragments/AidsfondsTransactions';

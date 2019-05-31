514eda10913ff86c3d402f64080e1d6967b9cb6f
fus <martynas@zimmermanzimmerman.nl>
2019-05-31 11:27:32 +0300

    Reformated tablechart export csv DONE.

--- src/app/components/charts/tablechart/TableChart.jsx
+++ src/app/components/charts/tablechart/TableChart.jsx
@@ -4,13 +4,19 @@ import PropTypes from 'prop-types';
 import MUIDataTable from 'mui-datatables';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
+/* utils */
+import { formatCsvData } from 'components/charts/tablechart/TableChart.util';
+
 /* consts */
-import { noData } from 'modules/visualizer/sort/container/fragments/TablechartFragment/TableChartFragment.const';
+import {
+  noData,
+  noDatCols
+} from 'modules/visualizer/sort/container/fragments/TablechartFragment/TableChartFragment.const';
 
 /* components */
 import getTheme from './TableChart.styles';
 import DownloadButton from 'components/Buttons/DownloadButton/DownloadButton';
-import { CSVDownload } from 'react-csv';
+import { CSVLink } from 'react-csv';
 
 // For a list of all options: https://github.com/gregnb/mui-datatables
 const options = {
@@ -35,7 +41,7 @@ const propTypes = {
 const defaultProps = {
   title: 'No title given',
   data: noData,
-  columns: [],
+  columns: noDatCols,
   options
 };
 
@@ -47,23 +53,44 @@ class TableChart extends React.Component {
       // so yeah basically we use these different
       // state variables for the different type
       // of export requested by AidsFonds
-      downloadDiffCsv: false,
       diffFormatedCsv: []
     };
 
+    this.exportBtn = React.createRef();
+
     this.handleDownload = this.handleDownload.bind(this);
+    this.clickLink = this.clickLink.bind(this);
   }
 
   handleDownload() {
-    console.log('this.props.data', this.props.data);
+    const diffFormatedCsv = formatCsvData(this.props.data);
+
+    this.setState({ diffFormatedCsv }, this.clickLink);
+  }
+
+  // so basically we need this workaround
+  // to use the CSVLink so that our file would have
+  // an actual name, not like with CSV Download
+  clickLink() {
+    if (
+      this.exportBtn &&
+      this.exportBtn.current &&
+      this.exportBtn.current.link &&
+      this.exportBtn.current.link.click &&
+      typeof this.exportBtn.current.link.click === 'function'
+    ) {
+      this.exportBtn.current.link.click();
+    }
   }
 
   render() {
     return (
       <MuiThemeProvider theme={getTheme()}>
-        {this.state.downloadDiffCsv && (
-          <CSVDownload data={this.state.diffFormatedCsv} target="_blank" />
-        )}
+        <CSVLink
+          data={this.state.diffFormatedCsv}
+          filename="tableDownload.csv"
+          ref={this.exportBtn}
+        />
         <MUIDataTable
           title={this.props.title}
           data={this.props.data}

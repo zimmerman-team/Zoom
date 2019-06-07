9d45306519c9147c593f38d94e339c484bd0a2f6
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-06-07 13:04:12 +0200

    refactor: moved tests to make better use of that parallelism

---
+++ cypress/integration/tests/chartbuilder/Barchart.spec.js
@@ -0,0 +1,52 @@
+describe('Chartbuilder bar chart fragment e2e', function() {
+  it('Should contain /barchart in the url and map aids related deaths data', function() {
+    cy.log('**Signs in and and navigates to barchart**');
+    cy.signIn();
+    cy.navigateToBarchart();
+
+    cy.log('**URL is correct**');
+    cy.url().should('include', '/visualizer/barchart');
+
+    cy.log('**Plots aids related deaths**');
+    cy.get(
+      '[class*=ExpansionPanelContainer]:nth-child(4) [data-cy="zoom-select"]'
+    )
+      .first()
+      .click();
+    cy.contains('aids related deaths (unaids)').click();
+    cy.waitPageLoader();
+    cy.get('[data-cy="legend-label"]').should('have.css', 'content');
+
+    cy.log('**Tooltip shows right content**');
+    cy.get('[data-cy="tooltip-info-button"]')
+      .scrollIntoView()
+      .trigger('mouseenter', { force: true });
+    cy.get('[data-cy="tooltip-content"]').should(
+      'have.text',
+      'Datasource: UNAIDS 2018'
+    );
+
+    cy.get('body').click();
+
+    cy.log('**Graph structure mutations works on barchart**');
+    cy.contains('Geolocation').click();
+    cy.contains('Year').click();
+    cy.wait(4000);
+    cy.get('rect')
+      .last()
+      .scrollIntoView()
+      .trigger('mouseover', { force: true });
+
+    cy.contains('Year: 2005');
+    cy.contains(
+      'aids related deaths (unaids) - adolescents (10 to 19) realistic estimate: 10000'
+    );
+  });
+
+  it('Should make a snapshot of the visual current state', function() {
+    cy.waitPageLoader();
+    cy.waitPageLoader2();
+    cy.wait(14000);
+    cy.percySnapshot('Chartbuilder - Barchart');
+  });
+});

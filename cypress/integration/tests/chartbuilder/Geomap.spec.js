e0d0a639db6f84a17c91a75f38e85b6081c6b3ca
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-06-07 13:04:12 +0200

    refactor: moved tests to make better use of that parallelism

---
+++ cypress/integration/tests/chartbuilder/Geomap.spec.js
@@ -0,0 +1,83 @@
+describe('Chartbuilder geomap chart fragment e2e', function() {
+  it('Should include /geomap in the url', function() {
+    cy.signIn();
+    cy.navigateToCreateGeo();
+    cy.url().should('include', '/visualizer/geomap');
+  });
+
+  it('Should make a snapshot of the visual current state', function() {
+    cy.waitPageLoader();
+    cy.waitPageLoader2();
+    cy.wait(15000);
+    cy.percySnapshot('Chartbuilder - Geomap');
+  });
+
+  it('Should pass written text from the /context to /preview', function() {
+    // Fixme: Change to proper url or simultate click of the button.
+    cy.waitPageLoader();
+    cy.get('[href="/visualizer/geomap/vizID/context"]').click();
+    cy.waitPageLoader();
+    cy.waitPageLoader2();
+    cy.get('textarea')
+      .last()
+      .type('This is a test');
+    cy.get('[href="/visualizer/geomap/vizID/preview"]').click();
+    cy.get('[data-cy="context-preview-intro"]').should(
+      'have.text',
+      'This is a test'
+    );
+  });
+
+  it('Should map aids related deaths data on the geo map', function() {
+    cy.signIn();
+    cy.navigateToCreateGeo();
+    //Here we wait till the indicators are loaded.
+    cy.wait(2000);
+    cy.get(
+      '[class*=ExpansionPanelContainer]:nth-child(4) [data-cy="zoom-select"]'
+    )
+      .first()
+      .click();
+    cy.contains('aids related deaths (unaids)').click();
+    //Here we wait till the data has been mapped
+    cy.waitPageLoader();
+    cy.wait(8000);
+    cy.get('[data-cy="legendLayer-label"]').should(
+      'contain',
+      'aids related deaths (unaids)'
+    );
+  });
+
+  // TODO: Implement this test.
+  // it('Should "save" the chart to the dashboard', function() {
+  //   navigateToCreateGeo();
+  //   cy.get('[data-cy="legendLayer-label"]').should(
+  //     'contain',
+  //     'aids related deaths'
+  //   );
+  //   cy.get('[data-cy=geomap-close-save-button]').click();
+  //   cy.visit('/visualizer/geomap/vizID/duplicate');
+  //   cy.get('[data-cy="duplicate-to-dashboard"]').click();
+  // });
+
+  //TODO: Implement when functionality has been implemented.
+  it('Should be able to download to csv/xml/json ', function() {
+    cy.signIn();
+    cy.navigateToCreateGeo();
+    cy.visit('/visualizer/geomap/vizID/download');
+    cy.waitPageLoader();
+    cy.waitPageLoader2();
+    cy.get('[data-cy="dowload-option-JSON"]').click();
+    cy.get('[data-cy="dowload-option-CSV"]').click();
+    cy.get('[data-cy="dowload-option-XML"]').click();
+  });
+
+  it('Should publish the chart to public zoom library', function() {
+    cy.signIn();
+    cy.navigateToCreateGeo();
+    cy.visit('/visualizer/geomap/vizID/visibility');
+    cy.get('[data-cy="publish-chart-to-public"]').click();
+    cy.visit('/public/chart-library');
+    // Fixme: Should check on the vizID in the url, however on creating a chart, the vizID is not in the URL yet.
+  });
+});
---
+++ cypress/integration/tests/chartbuilder/Tablechart.spec.js
@@ -0,0 +1,76 @@
+describe('Chartbuilder table chart fragment e2e', function() {
+  it('Should contain /tablechart in the url and map aids related deaths data', function() {
+    cy.signIn();
+    cy.navigateToTablechart();
+    cy.waitPageLoader2();
+    cy.waitPageLoader();
+    cy.url().should('include', '/visualizer/tablechart');
+
+    cy.get('[data-cy="year-2005"]').click();
+    //Here we wait till the indicators have loaded.
+    cy.wait(2000);
+    cy.get(
+      '[class*=ExpansionPanelContainer]:nth-child(4) [data-cy="zoom-select"]'
+    )
+      .first()
+      .click();
+
+    cy.contains('aids related deaths (unaids)').click();
+    cy.waitPageLoader();
+    cy.get('#MUIDataTableBodyRow-2 > :nth-child(5)').should('contain', '2005');
+    cy.get('#MUIDataTableBodyRow-2 > :nth-child(9)').should(
+      'contain',
+      'aids related deaths (unaids)'
+    );
+  });
+
+  it('Should make a snapshot of the visual current state', function() {
+    cy.waitPageLoader();
+    cy.waitPageLoader2();
+    cy.wait(2000);
+    cy.percySnapshot('Chartbuilder - Tablechart');
+  });
+
+  it('Should sort on Geolocation', function() {
+    cy.get(
+      ':nth-child(2) > .MUIDataTableHeadCell-toolButton > .MUIDataTableHeadCell-data'
+    ).click();
+    cy.waitPageLoader();
+    // cy.get('tbody>tr').should('contain', 'tonga'); fixme
+  });
+
+  it('Should only display Kenya data when searching "kenya"', function() {
+    cy.get('[aria-label="Search"]').click();
+    cy.get('.MuiInputBase-root > .MuiInputBase-input').type('kenya');
+    cy.waitPageLoader();
+    cy.get('tbody>tr').should('contain', 'kenya');
+  });
+
+  it('Should be able to delete Kenya data from table', function() {
+    cy.get('tbody>tr').within(() => {
+      cy.get('input[type="checkbox"]').click();
+    });
+    cy.get('[aria-label="Delete Selected Rows"]').click();
+    cy.waitPageLoader();
+    cy.get('tbody>tr').should('not.contain', 'Kenya');
+
+    cy.get('[aria-label="Search"]').click();
+    cy.get('.MUIDataTableSearch-main').within(() => {
+      cy.get('[type="button"]').click();
+    });
+  });
+
+  it('Resetting all values should populate data with No Data rows', function() {
+    cy.get('[data-cy="data-explorer-panel-reset"]').click();
+    cy.waitPageLoader();
+
+    cy.get('tbody>tr').should('contain', 'No Data');
+  });
+
+  it('Should make a snapshot of the visual current state', function() {
+    cy.waitPageLoader();
+    cy.waitPageLoader2();
+    cy.wait(2000);
+    cy.percySnapshot('Chartbuilder - Tablechart empty state');
+  });
+});

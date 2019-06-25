9b91bf92574de26f74efff33b2cc1a16fa8ac9ed
fus <martynas@zimmermanzimmerman.nl>
2019-06-25 17:50:13 +0300

    About test adjustment

--- src/app/__mocks__/userManagementMock.js
+++ src/app/__mocks__/userManagementMock.js
@@ -2,13 +2,13 @@ export const userManagementMockData = {
   roleOptions: [
     { label: 'Regular user', value: 'regular' },
     { label: 'Moderator', value: 'moderator' },
-    { label: 'Administrator', value: 'administrator' },
+    { label: 'Administrator', value: 'administrator' }
   ],
   orgOptions: [
     { label: 'Aidsfonds', value: 'aidsfonds' },
     { label: 'UNESCO', value: 'unesco' },
-    { label: 'ZZ', value: 'zz' },
-  ],
+    { label: 'ZZ', value: 'zz' }
+  ]
 };
 
 export { userManagementMockData as default };

b9a0c7b1ec77db233e3b82cca7ac84fac480e64c
fus <martynas@zimmermanzimmerman.nl>
2019-10-23 18:20:40 +0300

    chore: removed missed unused folder

--- src/app/components/MenuAppBar/MenuAppBar.story.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import MenuAppBar from './MenuAppBar';
-
-storiesOf('Components', module).add('MenuAppBar', () => <MenuAppBar />);

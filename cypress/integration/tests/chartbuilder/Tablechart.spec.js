1a1a33887496b2f0b8b0b8fded0d9f0f377d7366
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-11-13 13:46:03 +0100

    feat: implemented tablechart e2e test
    [ZOOM-2131]

--- cypress/integration/tests/chartbuilder/Tablechart.spec.js
+++ cypress/integration/tests/chartbuilder/Tablechart.spec.js
@@ -35,7 +35,6 @@ describe('Chartbuilder table chart fragment e2e', function() {
       ':nth-child(2) > .MUIDataTableHeadCell-toolButton > .MUIDataTableHeadCell-data'
     ).click();
     cy.waitPageLoader();
-    // cy.get('tbody>tr').should('contain', 'tonga'); fixme
   });
 
   it('Should only display Kenya data when searching "kenya"', function() {
@@ -71,5 +70,78 @@ describe('Chartbuilder table chart fragment e2e', function() {
     cy.waitPageLoader2();
     cy.wait(2000);
     cy.percySnapshot('Chartbuilder - Tablechart empty state');
+  });
+});
+
+const testvalues = {
+  indicator: "new hiv infections",
+  chartTitle: "cypress_test_chart",
+  chartTitleEdited: "edited_cypress_test_chart"
+}
+
+function plotData(){
+  cy.waitForIndicatorsLoad();
+  cy.get('[data-cy="indicator-1"]').click();
+  cy.contains(testvalues.indicator).click();
+
+  //Here we wait till the data has been mapped
+  cy.waitPageLoader();
+  cy.wait(16000);
+  cy.get('thead').should('contain', testvalues.indicator);
+}
+
+function typeChartTitle(title, fragment) {
+  cy.location().then((location) => {
+    cy.get(`[href="/visualizer/${fragment}/${location.pathname.split('/')[3]}/context"]`).click();
+    cy.waitPageLoader();
+    cy.waitPageLoader2();
+    cy.get('[class *= MuiInputBase-input]')
+      .eq(1)
+      .clear()
+      .type(title);
+  })
+}
+
+describe('Chartbuilder tablechart chart fragment e2e', function() {
+  it('Should be able to save, edit, preview, download and delete a chart', function() {
+    cy.navigateToTablechart();
+    cy.wait(5000);
+    cy.log("**PLOTTING CHART**");
+    plotData();
+    typeChartTitle(testvalues.chartTitle, "tablechart");
+
+    cy.log("**PREVIEW CHART**");
+    cy.get('[href="/visualizer/tablechart/vizID/preview"]').click();
+    cy.get('[class*= ContextHeader]').should(
+      'contain.text',
+      testvalues.chartTitle
+    );
+
+    cy.log("DOWNLOAD CHART");
+    cy.get('[href="/visualizer/tablechart/vizID/download"]').click();
+    cy.get('[data-cy="dowload-option-PNG"]').click();
+    //todo: test if file is actually being downloaded
+
+    cy.log("**SAVING CHART**");
+    cy.get('[data-cy=geomap-close-save-button]').click();
+    cy.wait(5000);
+    cy.get(":nth-child(1) > [class*= GridItemstyles]").first().should('contain.text', testvalues.chartTitle)
+
+    cy.log("**EDITING CHART**");
+    cy.wait(5000);
+    cy.get(":nth-child(1) > [class*= GridItemstyles]").first().trigger('mouseover');
+    cy.get("[class *= GridItemToolbar]:nth-child(1)").first().click();
+    cy.get('[data-cy="indicator-1"]').should('contain.text', testvalues.indicator);
+    typeChartTitle(testvalues.chartTitleEdited, "tablechart")
+
+    cy.get('[data-cy=geomap-close-save-button]').click();
+    cy.get(":nth-child(1) > [class*= GridItemstyles]").first().should('contain.text', testvalues.chartTitle)
+
+    cy.log("**DELETING CHART**");
+    cy.wait(5000);
+    cy.get(":nth-child(1) > [class*= GridItemstyles]").first().trigger('mouseover');
+    cy.get("[class *= GridItemToolbar]:nth-child(4)").first().click();
+    cy.wait(5000);
+    cy.queryByText(testvalues.chartTitleEdited).should("not.exist")
   });
 });

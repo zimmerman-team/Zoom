882754924a32e5b70e795f21584a8922495a2401
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-11-14 19:33:36 +0100

    test: implemented feedback country focus tests
    [ZOOM-2126]

--- cypress/integration/tests/chartbuilder/CountryFocus.spec.js
+++ cypress/integration/tests/chartbuilder/CountryFocus.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable func-names */
 const testvalues = {
-  chartTitle: "cypress_test_chart",
-  chartTitleEdited: "edited_cypress_test_chart"
-}
+  chartTitle: 'cypress_test_chart',
+  chartTitleEdited: 'edited_cypress_test_chart'
+};
 
 const indicatorValues = [];
 
@@ -24,7 +24,7 @@ function plotData(indicatorIndex) {
   cy.contains(indicatorValues[indicatorIndex]).click();
   //Here we wait till the data has been mapped
   cy.waitPageLoader();
-  cy.wait(20000);
+  cy.wait(1000);
   cy.get('[data-cy="legendLayer-label"]').should(
     'contain',
     indicatorValues[indicatorIndex]
@@ -40,7 +40,7 @@ function typeChartTitle(title, fragment) {
       .first()
       .clear()
       .type(title);
-  })
+  });
 }
 
 beforeEach(() => {
@@ -84,75 +84,32 @@ describe('Chartbuilder country focus NL fragment e2e', function() {
     cy.wait(5000);
     cy.get(":nth-child(1) > [class*= GridItemstyles]").first().trigger('mouseover');
     cy.get("[class *= GridItemToolbar]:nth-child(1)").first().click();
+    cy.wait(16000)
     cy.get('[data-cy="indicator-1"]').click();
     plotData(1)
+    typeChartTitle(testvalues.chartTitleEdited, "focusNL");
+    cy.get('[data-cy=geomap-close-save-button]').click();
+
+    cy.log('**CHECKING IF EDITS ARE SUCCESFULL**')
+    cy.get(':nth-child(1) > [class*= GridItemstyles]')
+      .first()
+      .should('contain.text', testvalues.chartTitleEdited)
+    cy.wait(5000);
+    cy.get(':nth-child(1) > [class*= GridItemstyles]')
+      .first()
+      .trigger('mouseover');
+    cy.get('[class *= GridItemToolbar]:nth-child(1)')
+      .first()
+      .click();
     cy.get('[data-cy="indicator-1"]').should(
       'contain.text',
       indicatorValues[1]
     );
-    typeChartTitle(testvalues.chartTitleEdited, "focusNL")
-
-    cy.get('[data-cy=geomap-close-save-button]').click();
-    cy.get(":nth-child(1) > [class*= GridItemstyles]").first().should('contain.text', testvalues.chartTitle)
-
-    cy.log("**DELETING CHART**");
-    cy.wait(5000);
-    cy.get(":nth-child(1) > [class*= GridItemstyles]").first().trigger('mouseover');
-    cy.get("[class *= GridItemToolbar]:nth-child(4)").first().click();
-    cy.wait(5000);
-    cy.queryByText(testvalues.chartTitleEdited).should("not.exist")
-
-    cy.log('**REMOVE CHARTS INDEFINITE**');
-    cy.get('[href="/dashboard/trash"]').click();
-    cy.queryByText(testvalues.chartTitleEdited).should('exist');
-    cy.get('[class *=GridListOptionstyles__RemoveButton]').click();
-    cy.wait(2000);
-    cy.queryByText(testvalues.chartTitleEdited).should('not.exist');
-
-  });
-});
-
-describe('Chartbuilder country focus KE fragment e2e', function() {
-  it('Should be able to save, edit, preview, download and delete a chart', function() {
-    cy.signIn();
-    cy.wait(2000);
-    cy.navigateToCountryFocusKenya();
-    cy.wait(5000);
-    cy.log("**PLOTTING CHART**");
-    plotData();
-    typeChartTitle(testvalues.chartTitle, "focusKE");
-
-    cy.log("**PREVIEW CHART**");
-    cy.get('[href="/visualizer/focusKE/vizID/preview"]').click();
-    cy.get('[class*= ContextHeader]').should(
-      'contain.text',
-      testvalues.chartTitle
-    );
-
-    cy.log("DOWNLOAD CHART");
-    cy.get('[href="/visualizer/focusKE/vizID/download"]').click();
-    cy.get('[data-cy="dowload-option-PNG"]').click();
-    //todo: test if file is actually being downloaded
-
-    cy.log("**SAVING CHART**");
-    cy.get('[data-cy=geomap-close-save-button]').click();
-    cy.wait(5000);
-    cy.get(":nth-child(1) > [class*= GridItemstyles]").first().should('contain.text', testvalues.chartTitle)
-
-    cy.log("**EDITING CHART**");
-    cy.wait(5000);
-    cy.get(":nth-child(1) > [class*= GridItemstyles]").first().trigger('mouseover');
-    cy.get("[class *= GridItemToolbar]:nth-child(1)").first().click();
-    cy.get('[data-cy="indicator-1"]').click();
-    plotData(1)
-    cy.get('[data-cy="indicator-1"]').should(
+    cy.get('[data-cy="legendLayer-label"]').should(
       'contain.text',
       indicatorValues[1]
     );
-    typeChartTitle(testvalues.chartTitleEdited, "focusKE")
-
     cy.get('[data-cy=geomap-close-save-button]').click();
-    cy.get(":nth-child(1) > [class*= GridItemstyles]").first().should('contain.text', testvalues.chartTitle)
 
     cy.log("**DELETING CHART**");
     cy.wait(5000);

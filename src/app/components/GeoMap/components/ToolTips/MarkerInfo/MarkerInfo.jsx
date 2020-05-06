fb2d98704b179152056bcaa443cc70f181008d0b
Stefanos <stefanos@zimmermanzimmerman.nl>
2020-05-06 12:51:49 +0300

    chore: remove console.log

--- src/app/components/GeoMap/components/ToolTips/MarkerInfo/MarkerInfo.jsx
+++ src/app/components/GeoMap/components/ToolTips/MarkerInfo/MarkerInfo.jsx
@@ -35,7 +35,6 @@ function getOffsetTop(hoverMarkerInfo) {
 // This component is specific for the react-map-gl, thus there's no story books
 // or unit tests for it as a seperate component
 const markerInfo = (hoverMarkerInfo) => {
-  console.log(hoverMarkerInfo);
   if (hoverMarkerInfo) {
     let countryName = hoverMarkerInfo.name;
     countryName = countryName.charAt(0).toUpperCase() + countryName.slice(1);
